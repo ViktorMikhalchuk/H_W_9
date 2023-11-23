99976b54109965a49633783acb5a1829c6f39cb5
Viktor Mikhalchuk <mihvi130780@gmail.com>
2023-11-23 15:43:41 +0200

    Home work 9

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,6 @@
 public class Application {
     public static void main(String[] args) {
-        // Приклад використання
+
         FileLoggerConfiguration config = new FileLoggerConfiguration("log.txt", LoggingLevel.DEBUG, 1024, "yyyy-MM-dd HH:mm:ss");
         FileLogger logger = new FileLogger(config);
 
@@ -8,7 +8,7 @@ public class Application {
         logger.debug("Debug message");
         logger.info("Info message");
 
-        // Приклад виклику методу, що викидає виняток
+        // виклик методу, що викидає виняток
         for (int i = 0; i < 20; i++) {
             logger.debug("Debug message " + i);
         }
